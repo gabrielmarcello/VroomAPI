ee6f4c30181f755fead2e98aef801a3a713d59f6
gabrielmarcello <gabriel.marcellocf@gmail.com>
2025-10-18 11:18:10 -0300

    Implementing Health Checks with UI dashbord

--- VroomAPI/Program.cs
+++ VroomAPI/Program.cs
@@ -1,17 +1,42 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
 using VroomAPI.Data;
 using VroomAPI.Interface;
 using VroomAPI.Mappings;
 using VroomAPI.Service;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("OracleConnection");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
+    options.UseOracle(connectionString));
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddHealthChecks()
+    .AddOracle(
+        connectionString: connectionString,
+        name: "oracle-database",
+        failureStatus: HealthStatus.Degraded,
+        tags: new[] { "db", "oracle", "sql" },
+        timeout: TimeSpan.FromSeconds(10)
+    ).AddUrlGroup(
+        uri: new Uri("http://127.0.0.1:1880"),
+        name: "node-red api",
+        failureStatus: HealthStatus.Degraded,
+        tags: new[] { "api", "node-red", "external" },
+        timeout: TimeSpan.FromSeconds(5)
+    );
+builder.Services.AddHealthChecksUI(opt =>
+{
+    opt.SetEvaluationTimeInSeconds(5);
+    opt.MaximumHistoryEntriesPerEndpoint(10);
+    opt.AddHealthCheckEndpoint("API Health Check", "/health");
+}).AddInMemoryStorage();
 
 builder.Services.AddScoped<ITagService, TagService>();
 builder.Services.AddScoped<IMotoService, MotoService>();
@@ -32,6 +57,17 @@ builder.Services.AddSwaggerGen(c => {
 });
 
 var app = builder.Build();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.MapHealthChecksUI(options =>
+{
+    options.UIPath = "/health-dashboard";
+});
+
 
 if (app.Environment.IsDevelopment())
 {
